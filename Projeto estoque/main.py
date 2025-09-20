f25dfbcc61e94957351777a5fd9e912b66caee36
João Victor <jv191207@gmail.com>
2025-09-20 20:34:16 -0300

    Versão final (Por enquanto...)

--- Projeto estoque/main.py
+++ Projeto estoque/main.py
@@ -2,31 +2,36 @@ from utils import adicionar_produto, sobrescrever_dados, menu, remover_produto
 import json
 
 while True:
+    # mostra o menu do sisteminha
     menu()
-    
+    # pergunta a opção desejada
     escolha = input('Informe a opção desejada: ').strip()
     try:
         escolha = int(escolha)
+        # abre o arquivo dos dados do estoque e carrega os dados do json para a lista_produto
         if escolha == 1: 
             try:
                 with open("Projeto estoque\\dados_estoque.json", 'r', encoding='utf-8') as dados_arquivos:
                     lista_produtos = json.load(dados_arquivos)
+        # caso o arquivo esteja vazio, ele cria a lista aqui
             except:
                 print('Arquivo vazio, lista criada')
                 lista_produtos = []
-                
+        # le a quantidade de produtos a serem adicionados   
             qnt_produtos = input('Quantidade de produtos desejada: ')
                 
             try:
                 qnt_produtos = int(qnt_produtos)
+        # o for faz o loop equivalente a quantidade de produtos
                 for i in range(qnt_produtos):
                     print(adicionar_produto(lista_produtos))
+        # sobrescreve os dados do arquivo mas agora com os novos produtos            
                 sobrescrever_dados(lista_produtos)
             except:
                 print('O valor informado é inválido')
                 
         elif escolha == 2:
-            remover_produto()
+            remover_produto(lista_produtos)
         elif escolha == 0:
             break
         else:
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Um projeto simples
+
+**O Foco**
+- Fiz esse sisteminha para por em prática alguns aprendizados meus, pude praticar com manipulação de arquivos e algo completamente novo, o JSON. Ainda to aprendendo sobre, mas uma hora tudo melhora
+
+Futuramente pretendo melhorar esse sistema em si ou reescrever do zero usando conceitos de orientação a objetos, coisa que ainda vou estudar. Também, durante a criação do projeto, percebi alguns erros triviais que cometi e pontos que preciso melhorar, como a modularização e organização do código
